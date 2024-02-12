c5b3706de6f1f0998ce937fbd4690396b03d7a94
Johannes Bechberger <johannes.bechberger@sap.com>
2024-02-12 17:46:54 +0100

    Implement tail-example

--- README.md
+++ README.md
@@ -1,52 +1,10 @@
-Sample BCC/Hello-ebpf project
-=============================
+System Call Logger
+==================
 
-Sample project using the bcc library of the 
-[hello-ebpf](https://github.com/parttimenerd/hello-ebpf) project
-to implement a trivial eBPF program in Java.
+Log system calls, focusing on `execve` and itimer calls, using Java and eBPF (via [hello-ebpf](https://github.com/parttimenerd/hello-ebpf/)).
 
-The [Main](src/main/java/sample/bcc/Main.java) class implements a simple eBPF program that counts
-the number of `execve` system calls per user:
-
-```java
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        try (var b = BPF.builder("""
-                BPF_HASH(counter_table);
-                
-                int hello(void *ctx) {
-                   u64 uid;
-                   u64 counter = 0;
-                   u64 *p;
-                
-                   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-                   p = counter_table.lookup(&uid);
-                   if (p != 0) {
-                      counter = *p;
-                   }
-                   counter++;
-                   counter_table.update(&uid, &counter);
-                   return 0;
-                }
-                """).build()) {
-            var syscall = b.get_syscall_fnname("execve");
-            b.attach_kprobe(syscall, "hello");
-            BPFTable.HashTable<Long, Long> counterTable = b.get_table("counter_table", UINT64T_MAP_PROVIDER);
-            while (true) {
-                Thread.sleep(2000);
-                for (var entry : counterTable.entrySet()) {
-                    System.out.printf("ID %d: %d\t", entry.getKey(), entry.getValue());
-                }
-                System.out.println();
-            }
-        }
-    }
-}
-```
-
-Read more on this specific example in my blog post 
-[Hello eBPF: Recording data in basic eBPF maps (2)](https://mostlynerdless.de/blog/2024/01/12/hello-ebpf-recording-data-in-basic-ebpf-maps-2/).
-
+This is based on the [HelloTail](https://github.com/parttimenerd/hello-ebpf/blob/main/bcc/src/main/java/me/bechberger/ebpf/samples/chapter2/HelloTail.java)
+example from the [hello-ebpf](https://github.com/parttimenerd/hello-ebpf/) project.
 
 Requirements
 ------------
@@ -79,13 +37,6 @@ java --enable-preview --enable-native-access=ALL-UNNAMED -jar target/sample-bcc-
 # or
 ./run.sh
 ```
-
-Other Examples
---------------
-For a recent blog post, I implemented a version of the 
-[HelloTail](https://github.com/parttimenerd/hello-ebpf/blob/main/bcc/src/main/java/me/bechberger/ebpf/samples/chapter2/HelloTail.java) 
-example from the [hello-ebpf](https://github.com/parttimenerd/hello-ebpf) repository
-as its own project. You can find it in the [tail-example](https://github.com/parttimenerd/sample-bcc-project/tree/tail-example) branch of this repository.
 
 Contributing
 ------------
--- src/main/java/sample/bcc/Main.java
+++ src/main/java/sample/bcc/Main.java
@@ -2,39 +2,112 @@ package sample.bcc;
 
 import me.bechberger.ebpf.bcc.BPF;
 import me.bechberger.ebpf.bcc.BPFTable;
-
-import static me.bechberger.ebpf.bcc.BPFTable.HashTable.UINT64T_MAP_PROVIDER;
+import me.bechberger.ebpf.bcc.Syscalls;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        try (var b = BPF.builder("""
-                BPF_HASH(counter_table);
-                
-                int hello(void *ctx) {
-                   u64 uid;
-                   u64 counter = 0;
-                   u64 *p;
-                
-                   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-                   p = counter_table.lookup(&uid);
-                   if (p != 0) {
-                      counter = *p;
-                   }
-                   counter++;
-                   counter_table.update(&uid, &counter);
-                   return 0;
+
+    record Arguments(boolean skipOthers) {
+        static Arguments parseArgs(String[] args) {
+            boolean skipOthers = false;
+            if (args.length > 0) {
+                if (args.length == 1 && args[0].equals("--skip-others")) {
+                    skipOthers = true;
+                } else {
+                    System.err.println("""
+                Usage: app [--skip-others]
+                    
+                   --skip-others: Only log execve and itimer system calls
+                """);
+                    System.exit(1);
                 }
-                """).build()) {
-            var syscall = b.get_syscall_fnname("execve");
-            b.attach_kprobe(syscall, "hello");
-            BPFTable.HashTable<Long, Long> counterTable = b.get_table("counter_table", UINT64T_MAP_PROVIDER);
-            while (true) {
-                Thread.sleep(2000);
-                for (var entry : counterTable.entrySet()) {
-                    System.out.printf("ID %d: %d\t", entry.getKey(), entry.getValue());
-                }
-                System.out.println();
             }
+            return new Arguments(skipOthers);
+        }
+    }
+
+    static final String EBPF_PROGRAM = """
+                BPF_PROG_ARRAY(syscall, 300);
+
+                int hello(struct bpf_raw_tracepoint_args *ctx) {
+                    int nr = ctx->args[1];
+                    // syscall.call(ctx, nr) doesn't compile
+                    // for whatever reason, so we use the raw helper
+                    // function instead
+                    bpf_tail_call_(bpf_pseudo_fd(1, -1), ctx, nr);
+                    bpf_trace_printk("Another syscall: %d", nr);
+                    return 0;
+                }
+
+                int hello_exec(void *ctx) {
+                    bpf_trace_printk("Executing a program");
+                    return 0;
+                }
+
+                int hello_timer(struct bpf_raw_tracepoint_args *ctx) {
+                    int nr = ctx->args[1];
+                    switch (nr) {
+                        case 222:
+                            bpf_trace_printk("Creating a timer");
+                            break;
+                        case 226:
+                            bpf_trace_printk("Deleting a timer");
+                            break;
+                        default:
+                            bpf_trace_printk("Some other timer operation");
+                            break;
+                    }
+                    return 0;
+                }
+
+                int ignore_nr(void *ctx) {
+                    return 0;
+                }
+                """;
+
+    public static void main(String[] args) {
+        run(Arguments.parseArgs(args));
+    }
+
+    static void run(Arguments args) {
+        try (var b = BPF.builder(EBPF_PROGRAM).build()) {
+            b.attach_raw_tracepoint("sys_enter", "hello");
+
+            var ignoreFn = b.load_raw_tracepoint_func("ignore_nr");
+            var execFn = b.load_raw_tracepoint_func("hello_exec");
+            var timerFn = b.load_raw_tracepoint_func("hello_timer");
+
+            var progArray = b.get_table("syscall", BPFTable.ProgArray.createProvider());
+            progArray.set(Syscalls.getSyscall("execve").number(), execFn);
+            for (String syscall : new String[]{
+                    "timer_create", 
+                    "timer_gettime", 
+                    "timer_getoverrun",
+                    "timer_settime", 
+                    "timer_delete"}) {
+                progArray.set(Syscalls.getSyscall(syscall).number(), timerFn);
+            }
+            // ignore some system calls that come up a lot
+            for (int i : new int[]{
+                    21, 22, 25, 29, 56, 57, 63, 64, 66, 72,
+                    73, 79, 98, 101, 115, 131,
+                    134, 135, 139, 172, 233, 280, 291}) {
+                progArray.set(i, ignoreFn);
+            }
+            b.trace_print(f -> {
+                String another = "Another syscall: ";
+                // replace other syscall with their names
+                if (f.line().contains(another)) {
+                    // skip these lines if --skip-others is passed
+                    if (args.skipOthers) {
+                        return null;
+                    }
+                    var syscall = Syscalls.getSyscall(
+                            Integer.parseInt(f.line().substring(
+                                    f.line().indexOf(another) + another.length())));
+                    return f.line().replace(another + syscall.number(), another + syscall.name());
+                }
+                return f.line();
+            });
         }
     }
 }
